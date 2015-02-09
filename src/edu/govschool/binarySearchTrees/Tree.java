a698f2cfc9094824f7c12dd1b3d021652fffb37d
Justin <justintime@verizon.net>
2015-02-09 09:34:13 -0500

    Edited package name to fit standard edu.govschool format

--- src/edu/govschool/binarySearchTrees/Tree.java
+++ src/edu/govschool/binarySearchTrees/Tree.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package binarysearch.trees;
+package edu.govschool.binarySearchTrees;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
